a9507250a77636f9770efcc7ea7cf39510cf802d
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-06-23 02:00:24 +0800

    add base class

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((500, 500))
+
+class Character:
+	def __init__(self, x, y, rect) -> None:
+		self.x = x
+		self.y = y
+		self.rect = rect
+	
+	def draw(self, surface):
+		surface.fill((255,255,255))
+
+	def move(self, dir: int, amt = 5):
+		# Note that dir is either the integer 1, meaning right, or -1, meaning left
+		self.x = self.x + dir*amt
