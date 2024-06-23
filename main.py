b35a527ddf687b01f0a3d7321b2c21a93765b94b
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-06-23 13:30:31 +0800

    Made movements smooth

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 pygame.init()
 
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((1000, 500))
 
 OBSTACLES = [((200, 375), (350, 410)), ((0, 100), (50, 120))]
 
@@ -30,7 +30,7 @@ class Character:
 		self.updateRect()
 		pygame.draw.rect(surface, (255, 0, 0), self.rect)
 
-	def move(self, dir: int, amt = 5):
+	def move(self, dir: int, amt = 0.5):
 		# Note that dir is either the integer 1, meaning right, or -1, meaning left
 		self.beginX = self.beginX + dir*amt
 		self.endX = self.endX + dir*amt
@@ -41,13 +41,13 @@ class Character:
 		allUnder = []
 		for obstacle in self.obstacles:
 			# Basically check if this obj is under by checking X coords to see if X is on platform then Y to see if y is on platform
-			allUnder.append(obstacle[0][0] <= self.endX <= obstacle[1][0] and \
-				obstacle[0][0] <= self.beginX <= obstacle[1][0] and \
+			allUnder.append((obstacle[0][0] <= self.endX <= obstacle[1][0] or \
+				obstacle[0][0] <= self.beginX <= obstacle[1][0]) and \
 				obstacle[0][1] <= self.endY <= obstacle[1][1])
 		
 		return any(allUnder) # Returns true if any of the obstacles are under the player
 	
-	def jump(self, amt = 5):
+	def jump(self, amt = 50):
 		if self.jumpCount > 0:
 			if self.checkUnder(): #Making sure not double jumping - basically chck if on platform
 				if self.beginY > 0:
@@ -59,7 +59,7 @@ class Character:
 
 		self.updateRect()
 	
-	def gravity(self, amt = 5):
+	def gravity(self, amt = 0.5):
 		if not self.checkUnder(): # NOthing underneath
 			if self.endY != 500:
 				self.endY += amt
@@ -78,19 +78,24 @@ def drawObstacles(surface, obstacles, color = (0,0,0)):
 
 player = Character(250, 350, 50, OBSTACLES)
 
-while True:
-	print(player.beginX, player.beginY)
-	print(player.endX, player.endY)
-	print(player.checkUnder())
-	print(player.jumpCount)
-	print()
+movements = {
+	'left': False,
+	'right': False
+}
 
+while True:
 	screen.fill((255,255,255))
 
-	# player.gravity()
+	player.gravity()
 
 	player.jump()
 	player.draw(screen)
+
+	# While keys down
+	if movements['left']:
+		player.move(-1)
+	if movements['right']:
+		player.move(1)
 
 	drawObstacles(screen, OBSTACLES)
 
@@ -104,8 +109,15 @@ while True:
 				if player.checkUnder():
 					player.jumpCount = 50
 			elif event.key == pygame.K_LEFT: # move left
-				player.move(-1)
+				movements['left'] = True
 			elif event.key == pygame.K_RIGHT: # move right
-				player.move(1)
+				movements['right'] = True
+		
+		# check if key up
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT:
+				movements['left'] = False
+			if event.key == pygame.K_RIGHT:
+				movements['right'] = False
 	
 	pygame.display.flip() # Refresh frame
